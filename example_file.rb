d1d2db726c892cd274df2976aec2ba4dd5bc87be
Jacky Lin <jacky.jia.lin@gmail.com>
2020-12-05 14:16:17 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,11 @@
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+greet("Steve")
+
+#greeting_a_person("Maria")
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
---
+++ learning-comments.rb
@@ -0,0 +1,6 @@
+run_code_inside = false
+puts "Code before if.. ends"
+ if run_code_inside
+    puts "code inside"
+ end 
+  puts "Code after it...end"
